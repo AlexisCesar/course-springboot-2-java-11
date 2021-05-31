d07364e69511d0a0e0520a08e92784f049e4dd4b
Alexis Cesar <alexis.cesar2017@outlook.com>
2021-05-31 10:48:03 -0300

    Project created

---
+++ src/test/java/com/alexiscesar/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexiscesar.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
