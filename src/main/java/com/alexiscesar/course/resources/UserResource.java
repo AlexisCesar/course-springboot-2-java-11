1332bebc600f493d4bb69dae3db04d75222f198f
Alexis Cesar <alexis.cesar2017@outlook.com>
2021-05-31 17:34:30 -0300

    User entity and resource

---
+++ src/main/java/com/alexiscesar/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.alexiscesar.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alexiscesar.course.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Alexis", "alexis@space.com", "999-999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
