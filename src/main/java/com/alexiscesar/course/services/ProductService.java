67079bd36b49228024aa7ff429490db6841d34bf
Alexis Cesar <alexis.cesar2017@outlook.com>
2021-06-04 00:04:47 -0300

    Product

---
+++ src/main/java/com/alexiscesar/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.alexiscesar.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexiscesar.course.entities.Product;
+import com.alexiscesar.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
